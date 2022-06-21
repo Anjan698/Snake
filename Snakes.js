e6a5947cbb3d90f2ee8e34a0eeb4cd7aa84392d5
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-06-20 21:19:06 -0400

    Add files via upload

--- Snakes.js
+++ Snakes.js
@@ -1,1 +1,10 @@
-
+import {SnakeLadder} from './snake_ladder';
+const Snake=document.getElementById('Snake')
+_SnakeObj=new SnakeLadder(Snake)
+Width =500
+height = 500
+ 
+Snake.Width=Width;
+Snake.height=height;
+ 
+console.log(_SnakeObj)
